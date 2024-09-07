1501fe933bd15a8697f5028304e8ec803f39f7ee
Abdul12majid <yisaabdulmajid@gmail.com>
2024-09-07 14:11:07 +0100

    staticfiles included

--- chat_app/views.py
+++ chat_app/views.py
@@ -4,4 +4,7 @@ from django.http import HttpRequest
 # Create your views here.
 
 def index(request):
-	return HttpResponse('Chat app !!!')
+	context = {
+
+	}
+	return render(request, 'index.html', context)
