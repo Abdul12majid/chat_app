7937ce0102372a0281a6319d9680c3d766e5079d
Abdul12majid <yisaabdulmajid@gmail.com>
2024-09-08 12:47:44 +0100

    create model chatmesage

--- chat_app/urls.py
+++ chat_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('detail/<int:pk>', views.detail, name='detail'),
     
 ]
--- chat_app/views.py
+++ chat_app/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render, HttpResponse
 from django.http import HttpRequest
+from .models import Profile, Friend
 
 # Create your views here.
 
 def index(request):
+	user = request.user.profile
+	friends = user.friends.all()
 	context = {
-
+		'user':user,
+		'friends':friends,
 	}
 	return render(request, 'index.html', context)
+
+
+def detail(request, pk):
+	user = request.user.profile
+	friend = Friend.objects.get(profile_id=pk)
+	context = {
+		'friend':friend,
+		'user':user,
+	}
+	return render(request, 'detail.html', context)
