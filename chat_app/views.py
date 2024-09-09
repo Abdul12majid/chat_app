8c9d3a565e16ce57a965b3e7aeae1cd30accde84
Abdul12majid <yisaabdulmajid@gmail.com>
2024-09-09 13:33:57 +0100

    message sent

--- chat_app/views.py
+++ chat_app/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from django.http import HttpRequest
-from .models import Profile, Friend
+from .models import Profile, Friend, ChatMessage
 from .forms import ChatMessageForm
 
 # Create your views here.
@@ -18,10 +18,23 @@ def index(request):
 def detail(request, pk):
 	user = request.user.profile
 	friend = Friend.objects.get(profile_id=pk)
+	profile = Profile.objects.get(id=friend.profile.id)
+	chats = ChatMessage.objects.all()
 	form = ChatMessageForm()
+	if request.method == "POST":
+		form = ChatMessageForm(request.POST)
+		if form.is_valid:
+			chat_message = form.save(commit=False)
+			chat_message.msg_sender = user
+			chat_message.msg_receiver = profile
+			chat_message.save()
+			#return redirect(request.META.get("HTTP_REFERER"))
+			return redirect("detail", pk=friend.profile.id)
 	context = {
 		'friend':friend,
 		'user':user,
 		'form':form,
+		'profile':profile,
+		'chats':chats,
 	}
 	return render(request, 'detail.html', context)
